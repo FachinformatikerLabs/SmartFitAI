d527114f411d911fa7a4d6dd87781e8e1687eecd
ThreshOffTheBoat <huyhang@web.de>
2024-01-29 14:36:33 +0100

    Änderung class Rezept

--- utils/nutritional_calculation.py
+++ utils/nutritional_calculation.py
@@ -1,7 +1,10 @@
 # Nährwertberechnungslogik
-
-def calculate_calories(self):
-    total_calories = 0
-    for ingredient, amount in self.ingredients.items():
-        total_calories += ingredient.calories * amount  
-    return total_calories
+class Recipe:
+    def __init__(self):
+        self.ingredients = {}  #dictionary um die zutaten und zutatenmenge zu speichern
+    
+    def calculate_calories(self):
+        total_calories = 0 #initialisiert gesmamten kalorien als variable
+        for ingredient, amount in self.ingredients.items(): #für jede zutat und menge aus dem ingredients dictionary
+            total_calories += ingredient.calories * amount  #addiert gesamt kaolirien mit der (kalorien einer zutat * menge)
+        return total_calories 
