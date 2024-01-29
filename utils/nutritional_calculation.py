8195eaa911a5561cee3dfd4da5a37349e5bd3cdd
ThreshOffTheBoat <huyhang@web.de>
2024-01-29 13:41:50 +0100

    Kalorien berechnen aus den Zutaten und der Menge aus dem Rezept

--- utils/nutritional_calculation.py
+++ utils/nutritional_calculation.py
@@ -1,1 +1,7 @@
 # Nährwertberechnungslogik
+
+def calculate_calories(self):
+    total_calories = 0
+    for ingredient, amount in self.ingredients.items():
+        total_calories += ingredient.calories * amount  
+    return total_calories
