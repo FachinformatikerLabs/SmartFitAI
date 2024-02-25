8a3f55df211f9251949f19dec8fe909ec5b7d539
Wab00se <danny.mohr@freenet.de>
2024-02-25 20:23:07 +0100

    Kivy um KivyMD erweitert. Vorhandene Views überarbeitet.

--- main.py
+++ main.py
@@ -3,49 +3,65 @@
 # Module, die man installieren muss, damit Kivy funktioniert
 # python -m pip install kivy[base] kivy_examples
 # python -m pip install pygame
+# pip install https://github.com/kivymd/KivyMD/archive/master.zip für KiviMD 2.0.1
 
-from kivy.app import App
+from kivymd.app import MDApp
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
-from kivy.uix.image import Image
-from kivy.uix.textinput import TextInput
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.pickers import MDDockedDatePicker
+from kivymd.uix.navigationrail import MDNavigationRailItem
+from kivy.properties import StringProperty
+
+class CommonNavigationRailItem(MDNavigationRailItem):
+   text = StringProperty()
+   icon = StringProperty()
 
 #Definieren der verschiedenen Screens:
-class Welcome(Screen):
+class Welcome(MDScreen):
    pass
 
-class CreateUser(Screen):
+class CreateUser(MDScreen):
+   pass
+   def checkbox_click(self, instances, value):
+      if value == True:
+         pass
+      else:
+         pass
+
+class Dashboard(MDScreen):
    pass
 
-class Dashboard(Screen):
+class RecipeSearch(MDScreen):
    pass
 
-class RecipeSearch(Screen):
+class IngredientSearch(MDScreen):
    pass
 
-class IngredientSearch(Screen):
-   pass
-
-class AllergenSearch(Screen):
+class AllergenSearch(MDScreen):
    pass
 
 class WindowManager(ScreenManager):
    pass
 
-# Loading multible .kv Design files
-Builder.load_file("pages/welcome.kv", encoding="utf8")
-Builder.load_file("pages/registration.kv", encoding="utf8")
-Builder.load_file("pages/dashboard.kv", encoding="utf8")
-Builder.load_file("pages/search.kv", encoding="utf8")
-'''Builder.load_file("pages/login.kv", encoding="utf8")'''
-
-class SmartFitAIApp(App):
+class SmartFitAIApp(MDApp):
    def build(self):
-      Window.size = (800,800)                                  # Window Size for all Views
-      Window.clearcolor = (85/255, 110/255, 83/255, 1)         # Window Color for all Views
+
+# Loading multible .kv Design files
+      Builder.load_file("pages/welcome.kv", encoding="utf8")
+      Builder.load_file("pages/registration.kv", encoding="utf8")
+      Builder.load_file("pages/dashboard.kv", encoding="utf8")
+      Builder.load_file("pages/search.kv", encoding="utf8")
+      '''Builder.load_file("pages/login.kv", encoding="utf8")'''
+
+# Definition verschiedner Layouts (Aktuell nur "Darkmode")
+      self.theme_cls.theme_style = "Dark"
+      self.theme_cls.primary_palette = "Darkgreen" 
+
+      Window.size = (1920,1080)
+
       sm = WindowManager()
       sm.add_widget(Welcome(name='Welcome'))
       sm.add_widget(CreateUser(name='NewUser'))
