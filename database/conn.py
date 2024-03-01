2af3f854e1cccfde0d708811ceb9e204cd4634b0
nyxb <contact@nyxb.email>
2024-03-01 13:34:27 +0100

    🧹 chore(.vscode/settings.json): Fügt "hashpw" zum Wörterbuch hinzu. ♻️ refactor(database/conn.py): Entfernt auskommentierten Code für Klarheit.

--- database/conn.py
+++ database/conn.py
@@ -1,10 +1,6 @@
 import os
 from supabase import create_client, Client
 
-# url: str = os.environ.get("https://jjhcgfkksochzqcqnbvo.supabase.co")
-# key: str = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqaGNnZmtrc29jaHpxY3FuYnZvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNzQ2OTczOCwiZXhwIjoyMDIzMDQ1NzM4fQ.38NmKB5_K6t-1squH4HuhP1G5pQdThGBNYUzewSN7As")
-# supabase: Client = create_client(url, key)
-
 url = "https://jjhcgfkksochzqcqnbvo.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqaGNnZmtrc29jaHpxY3FuYnZvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNzQ2OTczOCwiZXhwIjoyMDIzMDQ1NzM4fQ.38NmKB5_K6t-1squH4HuhP1G5pQdThGBNYUzewSN7As"
 
